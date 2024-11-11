03f41b0c43e21107409a35757dbd857b368cdd70
Alexander K <sashakinzalov_2002@mail.ru>
2024-11-11 06:57:45 +0300

    edit for new front

--- index.js
+++ index.js
@@ -1,47 +1,34 @@
 const express = require("express");
-const path = require('path');
-const bodyParser = require('body-parser')
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
-//const {initDb} = require("./DataBase/dao");
-//const {addClient} = require("./DataBase/add_client");
-const {TGsendMessage} = require("./TelegramBotConrol/botConrol");
-const {VKdolbit} = require("./Vk-bot/vk-bot-contol");
-const chatId = '-1001905292559';
-const token = '6377889590:AAEPyjV8bl0d-micYIc4xPfQjFKBgM9tsYg';
+const port = 5500;
 
-let vkBot = VKdolbit;
-app.use(express.static(`${__dirname}/../navyk-frontend-2024`));
+const { TGsendMessage } = require("./TelegramBotConrol/botConrol");
+
+const chatId = "-1001905292559";
+const token = "6377889590:AAEPyjV8bl0d-micYIc4xPfQjFKBgM9tsYg";
+
+app.use(express.static(`${__dirname}/../nvk-wbpg`));
 app.use(express.json());
 
-
-app.post('/api/callback', (req, res) => {
-/*const newClient = addClient(
- req.body.parent_name,
- req.body.parent_number,
- req.body.parent_mail,
- req.body.student_name,
- req.body.student_number,
- req.body.student_comment,
- req.body.student_class
-);*/
-const newMessage = TGsendMessage(
-req.body.parent_name,
- req.body.parent_number,
- req.body.student_name,
- req.body.student_number,
- req.body.student_comment,
- req.body.student_class
- );
-})
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '../navyk-frontend-2024/index.html'));
+app.post("/api/callback", (req, res) => {
+  const newMessage = TGsendMessage(
+    req.body.parent_name,
+    req.body.parent_number,
+    req.body.student_name,
+    req.body.student_number,
+    req.body.student_comment,
+    req.body.student_class
+  );
+});
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../nvk-wbpg/index.html"));
 });
 
 (async () => {
-    //await initDb();
-    app.listen(port, () => {
-        console.log(`Example app listening on port ${port}!`)
-    });
+  //await initDb();
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+  });
 })();
-
